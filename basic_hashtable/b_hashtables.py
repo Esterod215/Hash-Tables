a85199f8fd6112bd1bf73d0ec77da138f0fdd1f1
Estevan <estevan.rodriguez10@yahoo.com>
2019-07-09 10:35:20 -0700

    finished basic hashtables.py

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -16,7 +16,7 @@ class Pair:
 class BasicHashTable:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.count = 0
+        # self.count = 0
         self.storage = [None] * capacity
 
 
@@ -53,8 +53,13 @@ def hash_table_insert(hash_table, key, value):
 # If you try to remove a value that isn't there, print a warning.
 # '''
 def hash_table_remove(hash_table, key):
-    pass
-
+    index = hash(key, hash_table.capacity)
+    
+    if (hash_table.storage[index] is None or 
+            hash_table.storage[index].key != key):
+        print("Unable to remove item with key: " + key)
+    else:
+        hash_table.storage[index] = None
 
 # '''
 # Fill this in.
@@ -62,7 +67,14 @@ def hash_table_remove(hash_table, key):
 # Should return None if the key is not found.
 # '''
 def hash_table_retrieve(hash_table, key):
-    pass
+    index = hash(key, hash_table.capacity)
+    
+    if hash_table.storage[index] is not None:
+        if hash_table.storage[index].key == key:
+            return hash_table.storage[index].value
+    
+    print("Unable to find value with key " + key)
+    return None
 
 
 def Testing():
